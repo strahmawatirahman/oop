0b1b19d5a7654fa8e9ef166046560d741b75c33f
loveusomatchaa <rahmawatirhmn20@gmail.com>
2020-03-10 13:04:57 +0700

    tugas pertama

---
+++ soal2.java
@@ -0,0 +1,5 @@
+class dua{
+	public static void main (String[] args){
+		System.out.println("Belajar Bahasa Pemrograman Java");
+	}
+}
---
+++ soal3.java
@@ -0,0 +1,5 @@
+class tiga{
+	public static void main (String[] args){
+		System.out.println("Selesai Belajar Pemrograman Java");
+	}
+}
